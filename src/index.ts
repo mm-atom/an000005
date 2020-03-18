79526ebaab253f9fb793214d02dbb77c1cf3bebb
taoqf <tao_qiufeng@126.com>
2020-03-18 13:14:34 +0800

    dep

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import invoke from '@mmstudio/invoke';
-import config from '@mmstudio/config';
 
 interface IWebResult<T> {
 	data: T;
 }
 
 export default async function an5<T>(service: string, msg: unknown, actionid: string) {
-	const ret = await invoke<T | IWebResult<T>>(config.cwd, service, msg, actionid);
+	const ret = await invoke<T | IWebResult<T>>(service, msg, actionid);
 	if ((ret as IWebResult<T>).data) {
 		return (ret as IWebResult<T>).data;
 	}
